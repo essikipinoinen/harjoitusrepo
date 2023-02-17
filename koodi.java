d97f397c2f06f92600608ba6cb7fc2f0a6a1c500
essikipinoinen <81577252+essikipinoinen@users.noreply.github.com>
2023-02-17 09:37:59 +0200

    Feature/tekstintulostaminen (#5)
    * Lisättiin uusi metodi koodi.java -tiedostoon
    
    * Lisättiin rivi kuva.jpg -tiedostoon
    
    * Lisättiin tulostusmetodi (Hei Suomi!)
    
    * Toteutettiin tekstin tulostaminen
    
    * Lisättiin uusi metodi koodi.java -tiedostoon
    
    * Lisättiin rivi kuva.jpg -tiedostoon
    
    * En tiedä
    
    * Lisättiin uusi metodi koodi.java -tiedostoon

--- koodi.java
+++ koodi.java
@@ -5,3 +5,11 @@ Uusi rivi GitHubissa Develop-branchiin
 Tekoäly 1
 Tekoäly 2
 Tekoäly 3
+
+void tulostetaanHeiMaailma() {
+	System.out.printIn("Hei maailma!");
+}
+
+void tulostetaanHeiSuomi() {
+	System.out.printIn("Hei Suomi!");
+}
